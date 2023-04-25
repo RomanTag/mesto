e7727ad609df58a16adb7bb88dbd9de4573f0d08
Roman Valeev <valeevroman11@yandex.ru>
2023-04-25 16:56:42 +0300

    refactor: отрыть попап 2

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,12 @@
 // popups
 const popup = document.querySelector('.popup');
 const formElement = document.querySelector('.popup__input-container');
-const popupCloseBtn = document.querySelector('.popup__close-btn');
+const popupCloseBtn = popup.querySelector('.popup__close-btn');
+
 
 //popup 1 (edit profile)
 
-const popupEdit = document.querySelector('.popup__edit');
+// const popupEdit = document.querySelector('.popup__edit');
 const nameInput = document.querySelector('#name');
 const jobInput = document.querySelector('#description');
 
@@ -14,55 +15,67 @@ const jobInput = document.querySelector('#description');
 //popup 2 (edit add elements)
 
 const popupAdd = document.querySelector('.popup__add');
-const openPopupAddBtn = document.querySelector('.profile__add-btn');
+const popupAddCloseBtn = popupAdd.querySelector('.popup__close-btn');
+const popupAddOpenBtn = document.querySelector('.profile__add-btn');
 
 
 // profile
 
-const openPopupBtn = document.querySelector('.profile__edit-btn');
+const popupOpenPopupBtn = document.querySelector('.profile__edit-btn');
 const nameInputprofile = document.querySelector('.profile__name');
 const jobInfoProfile = document.querySelector('.profile__data');
 
 
+// полигон
+
+function createCards (nameValue, linkValue) {
+  const element = document.createElement('ul');
+  element.classList.add('element');
+
+  const artistElement = document.createElement('h4');
+  artistElement.classList.add('song__artist');
+  artistElement.textContent = artistvalue;
+
+}
+
+
+
+
+//полигон
+
+
 // открытие/закрытие попапа
 
-function openPopup() {
-  popupEdit.classList.add('popup__edit_opened');
-  nameInput.value = nameInputprofile.textContent;
-  jobInput.value = jobInfoProfile.textContent;
-  console.log(popupEdit);
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
 
-function closePopup() {
-  popupEdit.classList.remove('popup__edit_opened');
-  // popupSave.classList.add('popup_opened');
-  console.log(popupAdd);
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
-// popup 2
-
-function openPopupAdd() {
-  popupAdd.classList.add('popup__add_opened');
-  console.log(popupAdd);
-}
-
-
-function closePopupAdd() {
-  popupAdd.classList.remove('popup__add_opened');
-}
-
-// сохранить данные из input
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     nameInputprofile.textContent = nameInput.value;
     jobInfoProfile.textContent = jobInput.value;
-    closePopup();
+    closePopup(popup);
+    closePopup(popupAdd);
 }
 
 
-openPopupBtn.addEventListener('click', openPopup);
-popupCloseBtn.addEventListener('click', closePopup);
+popupOpenPopupBtn.addEventListener('click', () => {
+  openPopup(popup);
+  nameInput.value = nameInputprofile.textContent;
+  jobInput.value = jobInfoProfile.textContent;
+});
+
+popupCloseBtn.addEventListener('click', () => closePopup(popup));
+
 formElement.addEventListener('submit', handleFormSubmit);
-openPopupAddBtn.addEventListener('click', openPopupAdd);
+
+popupAddOpenBtn.addEventListener('click', () => openPopup(popupAdd));
+popupAddCloseBtn.addEventListener('click', () => closePopup(popupAdd));
+
+console.log();
