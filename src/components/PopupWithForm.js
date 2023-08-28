54f2b85e9e677972e8cf22c59068d11b3f4f4d2c
Roman Valeev <valeevroman11@yandex.ru>
2023-08-28 20:36:25 +0300

    refactor: ООП: Проектная работа 8 webpack

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,44 @@
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handlerSubmitForm) {
+    super(popupSelector);
+    this._formElement = this._popup.querySelector('.popup__form');
+    this._inputList = this._formElement.querySelectorAll('.popup__input');
+    this._handlerSubmitForm = handlerSubmitForm;
+  }
+
+  _getInputValues() {
+    this._inputValues = {};
+    this._inputList.forEach(input => {
+      this._inputValues[input.name] = input.value;
+    });
+    return this._inputValues;
+  }
+
+  getFormValues() {
+    return this._getInputValues();
+  }
+
+  getFormElement() {
+    return this._formElement;
+  }
+
+  close() {
+    this._formElement.reset();
+    super.close();
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._handlerSubmitForm(this._getInputValues());
+      this.close();
+    });
+  }
+
+  setSubmitHandler(handler) {
+    this._handlerSubmitForm = handler;
+  }
+}
